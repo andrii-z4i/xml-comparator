f5aae51cd11987299f97c7074bcca12ed7bfad62
Andrii Kozin <ankozin@microsoft.com>
2018-02-12 22:54:11 +0100

    Reorganise module

--- diff/xml_diff.py
+++ diff/xml_diff.py
@@ -1,18 +1,27 @@
 import xml.etree.ElementTree as ET
 from re import match, compile
+from logging import Logger
 
 _tag_regexp = compile("(\{.*\})(.*)")
+
 
 def get_name_from_tag(tag):
     return _tag_regexp.match(tag).group(2)
 
+
 class XmlComparator(object):
-    def __init__(self, file1, file2):
+    def __init__(self, file1, file2, logger=None):
         self._file1 = file1
         self._file2 = file2
         self._parsed_file1 = None
         self._parsed_file2 = None
         self._were_files_parsed = False
+        self._logger = logger
+
+    def _print_debug_information(self, message):
+        if not self._logger:
+            return
+        self._logger.debug(message)
 
     @property
     def is_parsed(self):
@@ -43,24 +52,23 @@ class XmlComparator(object):
     def _compare_two_elements(self, left_element, right_element):
         _left_name = left_element.text
         _right_name = right_element.text
-        _result = 1
-        if _left_name == _right_name:
-            _result = 0
-        elif _left_name < _right_name:
-            _result = -1
-        if _result != 0:
-            print('"', _left_name, '" <> "', _right_name, '" => ', _result)
+        _result = _left_name == _right_name
+
+        if not _result:
+            self._print_debug_information(
+                '"%s" <> "%s" => %s' % (_left_name, _right_name, _result))
         return _result
+
+    def _compare_elements_if_depth_reached(self, element_left, element_right):
+        if element_left is not None or element_right is not None:
+            return False
+        if element_right is None and element_left is None:
+            return True
+        return self._compare_two_elements(element_left, element_right)
 
     def _compare(self, element_left, element_right, index, depth=None):
         if depth == index:
-            if element_left is not None and element_right is None:
-                return 1
-            if element_left is None and element_right is not None:
-                return -1
-            if element_right is None and element_left is None:
-                return 0
-            return self._compare_two_elements(element_left, element_right)
+            return self._compare_elements_if_depth_reached(element_left, element_right)
 
         _left_sub_elements = self._sort_elements([e for e in element_left])
         _right_sub_elements = self._sort_elements([e for e in element_right])
@@ -69,7 +77,7 @@ class XmlComparator(object):
         _right_length = len(_right_sub_elements)
 
         if _left_length != _right_length:
-            return -1 if _left_length < _right_length else 1
+            return False
 
         if not _left_length:
             return self._compare_two_elements(element_left, element_right)
@@ -82,48 +90,20 @@ class XmlComparator(object):
         return self._single_result(_results)
 
     def _single_result(self, results):
-        _less_result = results.count(-1)
-        _more_result = results.count(1)
-        _result = 0
-        if _less_result and not _more_result:
-            _result = -1
-        elif _more_result and not _less_result:
-            _result = 1
-        elif _less_result and _more_result:
-            if _less_result == _more_result:
-                _result = -1
-            elif _less_result > _more_result:
-                _result = -1
-            elif _less_result < _more_result:
-                _result = 1
-        if _result != 0:
-            print(results, '==>', _result)
+        _false_index = None
+        try:
+            _false_index = results.index(False)
+        except ValueError as _ex:
+            _false_index = -1
+        _result = True
+        if _false_index != -1:
+            self._print_debug_information(results)
+            _result = False
         return _result
 
     def compare(self, depth=None):
         """Compare two files up to depth level"""
         index = 0
-        _result = self._compare(self._parsed_file1, self._parsed_file2, index, depth)
+        _result = self._compare(
+            self._parsed_file1, self._parsed_file2, index, depth)
         return _result
-
-
-from unittest import TestCase
-
-
-class TestXmlComparator(TestCase):
-
-    def test_parse_file(self):
-        c = XmlComparator('C:\\Users\\z4i\\projects\\investigations\\content.xml',
-                          'C:\\Users\\z4i\\projects\\investigations\\content2.xml')
-        c.parse()
-        self.assertTrue(c.is_parsed)
-        print(get_name_from_tag(c.xml_tree_file1.tag))
-        for child in c.xml_tree_file1:
-            print(get_name_from_tag(child.tag))
-        print(c.compare(depth=9))
-
-
-class TestElementWrapper(TestCase):
-
-    def test_regex(self):
-        self.assertEqual('xmap-content', get_name_from_tag('{urn:xmind:xmap:xmlns:content:2.0}xmap-content'))
